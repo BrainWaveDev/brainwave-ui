6d12aba846e813d670fb10677dc5813db114e663
JUNYI OU <49964599+Irvingouj@users.noreply.github.com>
2023-04-06 23:53:08 -0400

    file basically working

--- components/ui/FilesList/FilesList.tsx
+++ components/ui/FilesList/FilesList.tsx
@@ -3,12 +3,29 @@ import ActionsPopover from '@/components/ui/FilesList/ActionsPopover';
 import SearchIcon from '@/components/icons/SearchIcon';
 import classes from './FilesList.module.css';
 import { Document } from 'types';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 interface Props {
   documents: Document[];
 }
 
+const ONE_PAGE_SIZE = 5;
+
 export default function FilesList(props: Props) {
-  console.debug('FilesList', props.documents);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const totalPages = Math.floor(props.documents.length / ONE_PAGE_SIZE) ;
+
+  const splitArray = (array: Document[], chunkSize: number): Document[][] => {
+    const result: Document[][] = [];
+
+    for (let i = 0; i < array.length; i += chunkSize) {
+      const chunk = array.slice(i, i + chunkSize);
+      result.push(chunk);
+    }
+
+    return result;
+  };
+
   return (
     <section className="container px-4 mx-auto">
       <div className="sm:flex sm:items-center sm:justify-between">
@@ -38,7 +55,7 @@ export default function FilesList(props: Props) {
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
             <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+              <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700 table-fixed ">
                 <thead className="bg-gray-50 dark:bg-gray-800">
                   <tr>
                     <th
@@ -88,7 +105,7 @@ export default function FilesList(props: Props) {
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-                  {props.documents.map((document) => (
+                  {splitArray(props.documents, ONE_PAGE_SIZE)[currentPage].map((document) => (
                     DocumentRow(document)
                   ))}
                 </tbody>
@@ -100,7 +117,14 @@ export default function FilesList(props: Props) {
 
       <div className="flex items-center justify-between mt-6">
         <a
-          href="#"
+          onClick={
+            (e) => {
+              e.preventDefault()
+              if (currentPage > 0) {
+                setCurrentPage(currentPage - 1)
+              }
+            }
+          }
           className="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
         >
           <svg
@@ -118,56 +142,22 @@ export default function FilesList(props: Props) {
             />
           </svg>
 
-          <span>previous</span>
+          <span >previous</span>
         </a>
 
         <div className="items-center hidden md:flex gap-x-3">
-          <a
-            href="#"
-            className="px-2 py-1 text-sm text-blue-500 rounded-md dark:bg-gray-800 bg-blue-100/60"
-          >
-            1
-          </a>
-          <a
-            href="#"
-            className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-          >
-            2
-          </a>
-          <a
-            href="#"
-            className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-          >
-            3
-          </a>
-          <a
-            href="#"
-            className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-          >
-            ...
-          </a>
-          <a
-            href="#"
-            className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-          >
-            12
-          </a>
-          <a
-            href="#"
-            className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-          >
-            13
-          </a>
-          <a
-            href="#"
-            className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-          >
-            14
-          </a>
+          <PageIndexComponent totalPages={totalPages} setPage={setCurrentPage} currPage={currentPage}/>
         </div>
 
         <a
-          href="#"
+           onClick={
+            () => {
+              console.log("clicked next", currentPage, "total pages: ", totalPages)
+              if (currentPage < totalPages-1 ) {
+                setCurrentPage(currentPage + 1)
+              }
+            }
+          }
           className="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
         >
           <span>Next</span>
@@ -191,8 +181,58 @@ export default function FilesList(props: Props) {
     </section>
   );
 }
+
+function PageIndexComponent(props: { totalPages: number, setPage:  Dispatch<SetStateAction<number>>, currPage: number }) {
+
+  const pageNumberBox = (pageNumber: number, selected: boolean) => {
+    return (
+      <a
+        key={`page-${pageNumber}`}
+        className={`px-2 py-1 text-sm text-blue-500 rounded-md dark:bg-gray-800 ${selected ? "bg-blue-100/60" : ""}`}
+        onClick={(e) => {
+          e.preventDefault();
+          props.setPage(pageNumber);
+        }}
+      >
+        {pageNumber}
+      </a>
+    )
+  }
+
+  const ellipsis = () => (
+    <span key={`ellipsis_${Math.random()}`} className="px-2 py-1 text-sm text-gray-500">
+      ...
+    </span>
+  );
+
+  if (props.totalPages <= 5) {
+    // display all page numbers
+    return (
+      <>
+        {Array.from(Array(props.totalPages).keys()).map((pageNumber, index) => {
+          return pageNumberBox(pageNumber, pageNumber === props.currPage);
+        })}
+      </>
+    )
+  } else {
+    // display first page, last page, selected index, and ellipses
+    const isFirstPageSelected = props.currPage === 0;
+    const isLastPageSelected = props.currPage === props.totalPages ;
+    const pages = [
+      pageNumberBox(0, isFirstPageSelected),
+      !isFirstPageSelected && props.currPage - 1 >= 1 ? ellipsis() : null,
+      isFirstPageSelected || isLastPageSelected ? null : pageNumberBox(props.currPage, true),
+      !isLastPageSelected && props.totalPages - props.currPage >= 2 ? ellipsis() : null,
+      pageNumberBox(props.totalPages , isLastPageSelected),
+    ].filter(Boolean); // Remove null values from the array
+
+    return <>{pages}</>;
+  }
+}
+
+
 function DocumentRow(doc: Document) {
-  const formatBytes = (bytes: number): string =>{
+  const formatBytes = (bytes: number): string => {
     if (bytes < 1024) {
       return bytes + " B";
     } else if (bytes < 1048576) {
@@ -204,7 +244,7 @@ function DocumentRow(doc: Document) {
     }
   }
 
-  const formatDate = (dateString: string): string =>{
+  const formatDate = (dateString: string): string => {
     const date = new Date(dateString);
     const month = date.toLocaleString("default", { month: "long" });
     const day = date.getDate();
@@ -212,70 +252,72 @@ function DocumentRow(doc: Document) {
     return `${month}, ${day}, ${year}`;
   }
   return (
-  <tr key={doc.id}>
-    <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-      <div className="inline-flex items-center gap-x-3">
-        <input
-          type="checkbox"
-          className="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700" />
+    <tr key={doc.id}>
+      <td className="px-4 py-4 w-3/10 text-sm font-medium text-gray-700 whitespace-nowrap">
+        <div className="inline-flex items-center gap-x-3">
+          <input
+            type="checkbox"
+            className="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700" />
 
-        <div className="flex items-center gap-x-2">
-          <div className="flex items-center w-8 h-8 text-teal-400 bg-teal-50 rounded-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-5 h-5 mx-auto"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-            </svg>
-          </div>
+          <div className="flex items-center gap-x-2">
+            <div className="flex items-center w-8 h-8 text-teal-400 bg-teal-50 rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-5 h-5 mx-auto"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+              </svg>
+            </div>
 
-          <div>
-            <h2 className="font-normal text-gray-800 dark:text-white ">
-              {doc.name.split('/').pop()}
-            </h2>
-            <p className="text-xs font-normal text-gray-500 dark:text-gray-400">
-              {formatBytes(doc.metadata.size)}
-            </p>
+            <div>
+              <h2 className="font-normal text-gray-800 dark:text-white ">
+                {doc.name.split('/').pop()}
+              </h2>
+              <p className="text-xs font-normal text-gray-500 dark:text-gray-400">
+                {formatBytes(doc.metadata.size)}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </td>
-    <td className="px-12 py-4 text-sm font-normal text-gray-700 whitespace-nowrap">
-      {formatBytes(doc.metadata.size)}
-    </td>
-    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-      {formatDate(doc.metadata.lastModified)}
-    </td>
-    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-      {formatDate(doc.metadata.lastModified)}
-    </td>
-    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-      {doc.owner}
-    </td>
-    <td className="px-4 py-4 text-sm whitespace-nowrap">
-      <button className="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg dark:text-gray-300 hover:bg-gray-100">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
-        </svg>
-      </button>
-    </td>
-  </tr>);
+      </td>
+      <td className="px-12 py-4 text-sm font-normal text-gray-700 whitespace-nowrap">
+        {formatBytes(doc.metadata.size)}
+      </td>
+      <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+        {formatDate(doc.metadata.lastModified)}
+      </td>
+      <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+        {formatDate(doc.metadata.lastModified)}
+      </td>
+      <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+        {doc.owner}
+      </td>
+      <td className="px-4 py-4 text-sm whitespace-nowrap">
+        <button className="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg dark:text-gray-300 hover:bg-gray-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+          </svg>
+        </button>
+      </td>
+    </tr>);
 }
+
+
 
