5b59e000f8dda22c82b93bb0574e655a72a47b64
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-04-18 19:20:44 -0400

    Add code for querying relevant embeddings from the database

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,18 +1,104 @@
-import { ChatBody, Message } from '../../types/chat';
-import { DEFAULT_SYSTEM_PROMPT } from '@/utils/app/const';
+import { Message, RequestBody } from '../../types/chat';
+import { defaultPrompt } from '@/utils/app/const';
 import { OpenAIError, OpenAIStream } from '@/utils/server';
 import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
 import { Tiktoken, init } from '@dqbd/tiktoken/lite/init';
 // @ts-expect-error
 import wasm from '../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module';
+import GPT3Tokenizer from 'gpt3-tokenizer';
+import { createClient } from '@supabase/supabase-js';
 
 export const config = {
   runtime: 'edge'
 };
 
+const openAIApiKey = process.env.OPENAI_API_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
 const handler = async (req: Request): Promise<Response> => {
   try {
-    const { model, messages, key, prompt } = (await req.json()) as ChatBody;
+    if (!openAIApiKey)
+      throw new Error('Missing environment variable OPENAI_API_KEY');
+    if (!supabaseUrl)
+      throw new Error('Missing environment variable SUPABASE_URL');
+    if (!supabaseServiceKey)
+      throw new Error('Missing environment variable SUPABASE_SERVICE_ROLE_KEY');
+
+    const { jwt, model, messages } = (await req.json()) as RequestBody;
+    const userQuestion = messages.at(messages.length - 1);
+
+    if (!jwt) throw new Error('Missing access token in request data');
+    if (!userQuestion) throw new Error('Missing query in request data');
+
+    const supabase = createClient(supabaseUrl, supabaseServiceKey);
+
+    const {
+      data: { user }
+    } = await supabase.auth.getUser(jwt);
+    if (!user)
+      return new Response('Error', {
+        status: 401,
+        statusText:
+          'The user does not have an active session or is not authenticated'
+      });
+
+    const sanitizedQuery = userQuestion.content.trim();
+
+    const embeddingResponse = await fetch(
+      'https://api.openai.com/v1/embeddings',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${openAIApiKey}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          model: 'text-embedding-ada-002',
+          input: sanitizedQuery.replaceAll('\n', ' ')
+        })
+      }
+    );
+
+    if (embeddingResponse.status !== 200) {
+      throw new Error('Failed to create embedding for question');
+    }
+
+    const {
+      data: [{ embedding }]
+    } = await embeddingResponse.json();
+
+    const { error: matchError, data: documentChunks } = await supabase.rpc(
+      'match_document_chunks',
+      {
+        embedding,
+        match_count: 10,
+        match_threshold: 0.9,
+        min_content_length: 50
+      }
+    );
+
+    '{\n' + '    "text": "."\n' + '}';
+
+    if (matchError) {
+      console.error(matchError);
+      throw new Error('Failed to match document chunks');
+    }
+
+    const tokenizer = new GPT3Tokenizer({ type: 'gpt3' });
+    let tokenCount = 0;
+    let contextText = '';
+
+    for (let i = 0; i < documentChunks.length; i++) {
+      const documentChunk = documentChunks[i];
+      const content = documentChunk.content;
+      const encoded = tokenizer.encode(content);
+      tokenCount += encoded.text.length;
+
+      if (tokenCount >= 1500) break;
+
+      contextText += `${content.trim()}\n---\n`;
+    }
 
     await init((imports) => WebAssembly.instantiate(wasm, imports));
     const encoding = new Tiktoken(
@@ -21,16 +107,9 @@ const handler = async (req: Request): Promise<Response> => {
       tiktokenModel.pat_str
     );
 
-    let promptToSend = prompt;
-    if (!promptToSend) {
-      promptToSend = DEFAULT_SYSTEM_PROMPT;
-    }
+    let promptToSend = defaultPrompt(contextText);
 
-    const prompt_tokens = encoding.encode(promptToSend);
-
-    let tokenCount = prompt_tokens.length;
     let messagesToSend: Message[] = [];
-
     for (let i = messages.length - 1; i >= 0; i--) {
       const message = messages[i];
       const tokens = encoding.encode(message.content);
@@ -44,15 +123,20 @@ const handler = async (req: Request): Promise<Response> => {
 
     encoding.free();
 
-    const stream = await OpenAIStream(model, promptToSend, key, messagesToSend);
+    const stream = await OpenAIStream(model, promptToSend, messagesToSend);
 
     return new Response(stream);
   } catch (error) {
     console.error(error);
-    if (error instanceof OpenAIError) {
-      return new Response('Error', { status: 500, statusText: error.message });
+    if (error instanceof OpenAIError || error instanceof Error) {
+      return new Response('Error', { status: 400, statusText: error.message });
     } else {
-      return new Response('Error', { status: 500 });
+      // Print out unexpected errors as is to help with debugging
+      console.error(error);
+      return new Response('Error', {
+        status: 500,
+        statusText: 'There was an error processing your request'
+      });
     }
   }
 };
