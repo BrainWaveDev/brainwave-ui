b803168c0a4bb3be41b4cae5fd4d763cef15efaf
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-04-19 01:04:03 -0400

    Update prompt to include references to original documents

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,4 +1,4 @@
-import { codeBlock } from 'common-tags';
+import { codeBlock, oneLine } from 'common-tags';
 
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.DEFAULT_SYSTEM_PROMPT ||
@@ -8,17 +8,24 @@ export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
 
 export const defaultPrompt = (contextText: string = '') => codeBlock`
-      ${`
+      ${
+        oneLine`
         You are an intelligent language model that answers questions using
         the information from the user's documents. Given the following sections 
         from the user's documents, answer the question using only that information,
-        outputted in markdown format. Include references to the context from where 
-        you got the information. If you are unsure and the answer
-        is not explicitly written in the documentation, say
-        "Unfortunately, I wasn't able to find relevant information in your documents. 
-        Here is my generic response:\n\n" and provided a generic response. Respond as 
-        markdown. 
-      `}
+        outputted in markdown format. If you are unsure about the answer, the answer is not explicitly 
+        written in the user's documents or no context was provided, say` +
+        `\n"Unfortunately, I wasn't able to find relevant information in your documents. Here is my generic response:"\n\n` +
+        oneLine`and provide a generic response. If the context was provided and 
+        your based your answer on the context, then include name of the document from where you
+        got the information in the brackets in the following format: "Source: {document name}".
+        Respond as markdown. 
+      `
+      }
 
-      ${contextText.length > 0 && `Context sections:\n${contextText}`}
+      ${
+        contextText.length > 0
+          ? `Context sections:\n\n${contextText}`
+          : 'No context.'
+      }
       `;
