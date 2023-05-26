6dc123b575f85e994d43e4c26e180c2a37863487
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-05-26 17:42:56 -0400

    Update ChatbarSettings.tsx

--- components/Chatbar/ChatbarSettings.tsx
+++ components/Chatbar/ChatbarSettings.tsx
@@ -1,36 +1,9 @@
-import { IconMoon, IconSun } from '@tabler/icons-react';
-import { useTranslation } from 'next-i18next';
-import { FC } from 'react';
-import { SidebarButton } from '../Sidebar/SidebarButton';
 import { ClearConversations } from './ClearConversations';
-import { useAppSelector } from 'context/redux/store';
 
-interface Props {
-  conversationsCount: number;
-  onToggleLightMode: (mode: 'light' | 'dark') => void;
-}
-
-export const ChatbarSettings: FC<Props> = ({
-  conversationsCount,
-  onToggleLightMode,
-}) => {
-  const { t } = useTranslation('sidebar');
-  const lightMode = useAppSelector(state => state.lightmode).mode
+export const ChatbarSettings = () => {
   return (
-    <div className="flex flex-col items-center space-y-1 border-t border-white/20 pt-1 text-sm">
-      {conversationsCount > 0 ? (
-        <ClearConversations />
-      ) : null}
-
-      <SidebarButton
-        text={lightMode === 'light' ? t('Dark mode') : t('Light mode')}
-        icon={
-          lightMode === 'light' ? <IconMoon size={18} /> : <IconSun size={18} />
-        }
-        onClick={() =>
-          onToggleLightMode(lightMode === 'light' ? 'dark' : 'light')
-        }
-      />
+    <div className="flex flex-col items-center space-y-1 pt-1 text-sm">
+      <ClearConversations />
     </div>
   );
 };
