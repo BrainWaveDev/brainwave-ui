76890fb11bd6f9467b7e39d9c4f711587b228240
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-05-26 23:39:36 -0400

    Update useThemeDetector hook

--- hooks/useThemeDetector.tsx
+++ hooks/useThemeDetector.tsx
@@ -1,33 +1,41 @@
-import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import { useEffect } from 'react';
+import { useAppDispatch } from '../context/redux/store';
+import { setTheme } from '../context/redux/themeSlice';
 
-export default function useThemeDetector(): [
-  boolean,
-  Dispatch<SetStateAction<boolean>>
-] {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+type Theme = 'dark' | 'light';
+
+/**
+ * Detects the user's preferred theme and sets it in the Redux store.
+ * Attaches a listener to the browser's media query for dark mode to
+ * update theme based on the browser's theme.
+ */
+export default function useThemeDetector() {
+  const dispatch = useAppDispatch();
+
   const mqListener = (e: MediaQueryListEvent) => {
     if (document && document.documentElement) {
-      document.documentElement.setAttribute(
-        'data-theme',
-        e.matches ? 'dark' : 'light'
-      );
-      setIsDarkTheme(e.matches);
+      const theme = e.matches ? 'dark' : 'light';
+      document.documentElement.setAttribute('data-theme', theme);
+      dispatch(setTheme(theme as Theme));
     }
   };
 
   useEffect(() => {
+    // Read theme from local storage
+    let preferredTheme = localStorage.getItem('preferredTheme');
+    // Read document media query
     const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)');
+    // If no preferred theme is set, use the system preference
+    if (!preferredTheme)
+      preferredTheme = darkThemeMq.matches ? 'dark' : 'light';
 
     // Set browser theme after page loads
-    document.documentElement.setAttribute(
-      'data-theme',
-      darkThemeMq.matches ? 'dark' : 'light'
-    );
-    setIsDarkTheme(darkThemeMq.matches);
+    document.documentElement.setAttribute('data-theme', preferredTheme);
+    // Update Redux store
+    dispatch(setTheme(preferredTheme as Theme));
 
     // Listen for changes to browser theme
     darkThemeMq.addEventListener('change', mqListener);
     return () => darkThemeMq.removeEventListener('change', mqListener);
   }, []);
-  return [isDarkTheme, setIsDarkTheme];
 }
