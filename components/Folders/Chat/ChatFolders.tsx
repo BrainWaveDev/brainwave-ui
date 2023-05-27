d2ece85b6af7f42e8260d8b2f3542187b8e84731
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-05-26 22:42:06 -0400

    Update ChatFolders.tsx

--- components/Folders/Chat/ChatFolders.tsx
+++ components/Folders/Chat/ChatFolders.tsx
@@ -1,28 +1,23 @@
-import { ConversationSummary } from '../../../types/chat';
-import { Folder } from '../../../types/folder';
 import { FC } from 'react';
 import { ChatFolder } from './ChatFolder';
+import { getFoldersFromStorage } from '../../../context/redux/folderSlice';
 
 interface Props {
   searchTerm: string;
-  conversations: ConversationSummary[];
-  folders: Folder[];
-  // conversation props
 }
 
-export const ChatFolders: FC<Props> = ({
-  searchTerm,
-  conversations,
-  folders,
-  // conversation props
-}) => {
+export const ChatFolders: FC<Props> = ({ searchTerm }) => {
+  // =======================
+  // Redux State
+  // =======================
+  const folders = getFoldersFromStorage();
+
   return (
     <div className="flex w-full flex-col pt-2">
       {folders.map((folder, index) => (
         <ChatFolder
           key={index}
           searchTerm={searchTerm}
-          conversations={conversations.filter((c) => c.folderId)}
           currentFolder={folder}
         />
       ))}
