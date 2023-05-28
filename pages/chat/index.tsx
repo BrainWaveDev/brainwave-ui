daf39823c766ecc63d9e9f77f765b1268581a4b1
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-05-28 00:29:43 -0400

    Update index.tsx

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -1,100 +1,16 @@
-import { Chat } from '@/components/Chat/Chat';
-import { Chatbar } from '@/components/Chatbar/Chatbar';
-import {
-  Conversation,
-  Message
-} from '../../types/chat';
-import {
-  cleanConversationHistory
-} from '@/utils/app/clean';
-import { Document } from '@/types/document';
-import { GetServerSideProps } from 'next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Chat from '@/components/Chat/Chat';
+import { Conversation } from '@/types/chat';
+import { cleanConversationHistory } from '@/utils/app/clean';
 import Head from 'next/head';
-import { useEffect, useRef, useState } from 'react';
-import { useSessionContext } from '@supabase/auth-helpers-react';
-import { useAppDispatch, useAppSelector } from 'context/redux/store';
-import { OpenAIModelID, fallbackModelID } from 'types/openai';
-import { optimisticConversationsActions, setConversations } from 'context/redux/conversationsSlice';
-import { optimisticDocumentActions } from 'context/redux/documentSlice';
-import { optimisticFoldersAction } from 'context/redux/folderSlice';
-import { setLightMode } from 'context/redux/lightmodeSlice';
-import { getDocumentListServerSideProps } from '@/utils/supabase-admin';
+import { useEffect, useRef } from 'react';
+import { initStore } from 'context/redux/store';
+import { setConversations } from 'context/redux/conversationsSlice';
 
-interface ChatProps {
-  defaultModelId: OpenAIModelID;
-  documents: Document[];
-  error?: string;
-}
-
-// This interface is used to pass document information to the filter component
-export interface DocumentInfo {
-  id: number;
-  name: string;
-  selected: boolean;
-}
-
-const ChatUI: React.FC<ChatProps> = () => {
-
-  // STATE ----------------------------------------------
-
-  // const [selectedConversation, setSelectedConversation] = useState<Conversation>();
-
-
-  const [showSidebar, setShowSidebar] = useState<boolean>(false);
-
-  const { isLoading, session, error } = useSessionContext();
-
-  const lightmode = useAppSelector((state) => state.lightmode);
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    // loading after login
-    if (isLoading || error) {
-      return;
-    }
-    const user_id = session?.user?.id!;
-    // when swich beteen pages, the redux store will be empty, so we need to fetch data from supabase aging, need to do something about this
-    dispatch(optimisticFoldersAction.fetchAllFolders(user_id))
-    dispatch(optimisticConversationsActions.initAllConversations(user_id));
-    dispatch(optimisticDocumentActions.fetchAllDocuments(user_id));
-  }, [isLoading, error]);
-
-  // REFS ----------------------------------------------
-
-  const containerRef = useRef<HTMLDivElement>(null);
+const ChatUI = () => {
+  // ========= Element References =========
   const stopConversationRef = useRef<boolean>(false);
 
-
-  // BASIC HANDLERS --------------------------------------------
-
-  const handleLightMode = (mode: 'dark' | 'light') => {
-    dispatch(setLightMode(mode))
-    localStorage.setItem('theme', mode);
-  };
-
-  const handleToggleChatbar = () => {
-    setShowSidebar(!showSidebar);
-    localStorage.setItem('showChatbar', JSON.stringify(!showSidebar));
-  };
-
-
-  // ON LOAD --------------------------------------------
-
   useEffect(() => {
-    const theme = localStorage.getItem('theme');
-    if (theme) {
-      setLightMode(theme as 'dark' | 'light');
-    }
-
-    if (window.innerWidth < 640) {
-      setShowSidebar(false);
-    }
-
-    const showChatbar = localStorage.getItem('showChatbar');
-    if (showChatbar) {
-      setShowSidebar(showChatbar === 'true');
-    }
-
     const conversationHistory = localStorage.getItem('conversationHistory');
     if (conversationHistory) {
       const parsedConversationHistory: Conversation[] =
@@ -104,7 +20,6 @@ const ChatUI: React.FC<ChatProps> = () => {
       );
       setConversations(cleanedConversationHistory);
     }
-
   }, []);
 
   return (
@@ -118,51 +33,10 @@ const ChatUI: React.FC<ChatProps> = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={`flex h-[calc(100vh_-_4rem)] min-w-full flex-col text-sm text-white dark:text-white ${lightmode}`}
-      >
-        <div
-          className="flex h-full w-full pt-0 relative"
-          ref={containerRef}
-        >
-          <Chatbar
-            showSidebar={showSidebar}
-            handleToggleChatbar={handleToggleChatbar}
-            onToggleLightMode={handleLightMode}
-          />
-          <div className="flex flex-1 w-full">
-            <Chat
-              stopConversationRef={stopConversationRef}
-            />
-          </div>
-        </div>
-      </div>
+      <Chat stopConversationRef={stopConversationRef} />
     </>
   );
 };
 
 export default ChatUI;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const defaultModelId =
-    (process.env.DEFAULT_MODEL &&
-      Object.values(OpenAIModelID).includes(
-        process.env.DEFAULT_MODEL as OpenAIModelID
-      ) &&
-      process.env.DEFAULT_MODEL) ||
-    fallbackModelID;
-
-  const props = {
-    defaultModelId,
-    ...(await serverSideTranslations(context.locale ?? 'en', [
-      'common',
-      'chat',
-      'sidebar',
-      'markdown',
-      'promptbar'
-    ]))
-  };
-
-  // Combine with the props containing the document list
-  return getDocumentListServerSideProps(context, props);
-};
+export const getServerSideProps = initStore;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,10 @@
 import FileInput from '@/components/ui/FileInput/FileInput';
 import FilesList from '@/components/ui/FilesList';
-import { useEffect, useState } from 'react';
-import { RotatingLines } from 'react-loader-spinner';
-import { ErrorAlert, useErrorContext } from '../context/ErrorContext';
-import { useAppDispatch, useAppSelector } from 'context/redux/store';
-import { useSessionContext } from '@supabase/auth-helpers-react';
-import { optimisticDocumentActions } from 'context/redux/documentSlice';
-export default function HomePage({
-}: {
-  error?: string;
-}) {
-  const { isLoading, session, error } = useSessionContext();
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    if (isLoading || error) {
-      return;
-    }
-    // client fetch for redux, since SEO is not important here
-    const user = session?.user;
-    dispatch(optimisticDocumentActions.fetchAllDocuments(user?.id!))
-  }, [isLoading, error]);
+import { initStore } from 'context/redux/store';
+import { useErrorContext } from '../context/ErrorContext';
 
+export default function HomePage({ error }: { error: string | null }) {
+  const { errorDispatch } = useErrorContext();
   return (
     <>
       <header className="bg-white shadow">
@@ -34,20 +18,10 @@ export default function HomePage({
         <FileInput />
       </div>
       <div className="mx-auto max-w-7xl pt-2 pb-6 sm:px-6 lg:px-8">
-        {isLoading ? (
-          <section className="container px-4 mx-auto flex items-center place-content-center h-[45vh]">
-            <RotatingLines
-              strokeColor="#9ca3af"
-              strokeWidth="1.5"
-              animationDuration="1"
-              width="4.5rem"
-              visible={true}
-            />
-          </section>
-        ) : (
-          <FilesList/>
-        )}
+        <FilesList />
       </div>
     </>
   );
 }
+
+export const getServerSideProps = initStore;
