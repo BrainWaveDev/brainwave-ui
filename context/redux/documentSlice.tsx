85cd04b72456d0062e2a948ebdf28fa73a5ee686
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-05-27 23:36:18 -0400

    Update documentSlice.tsx

--- context/redux/documentSlice.tsx
+++ context/redux/documentSlice.tsx
@@ -1,67 +1,76 @@
-'use client'
-import { createSlice } from '@reduxjs/toolkit'
-import { AppThunk } from './store'
-import { deleteDocuments, fetchAllDocuments } from '@/utils/app/documents'
-import type { PayloadAction } from '@reduxjs/toolkit'
-import { Document } from '@/types/document'
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import { AppThunk, useAppSelector } from './store';
+import { deleteDocuments, fetchAllDocuments } from '@/utils/app/documents';
+import { Document } from '@/types/document';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { endLoading, LoadingTrigger, startLoading } from './loadingSlice';
 
-type DocumentState = Document[]
+type DocumentState = Document[];
 
-const initialState = [] as DocumentState
+const initialState: DocumentState = [];
 
 const documentSlice = createSlice({
-    name: 'documents',
-    initialState,
-    reducers: {
-        setAllDocuments: (state, action) => {
-            return action.payload
-        },
-        deleteDocuments: (state, action: PayloadAction<number[]>) => {
-            return state.filter(document => !action.payload.includes(document.id))
-        },
-        addDocument: (state, action: PayloadAction<Document>) => {
-            return [...state, action.payload]
-        },
-        addAllDocuments: (state, action: PayloadAction<Document[]>) => {
-            return [...state, ...action.payload]
-        }
+  name: 'documents',
+  initialState,
+  reducers: {
+    setAllDocuments: (state, action) => {
+      return action.payload;
     },
-})
-
-const thunkAddAllDocuments = (user_id: string): AppThunk => {
-    return async (dispatch, getState) => {
-        try {
-            const documents = await fetchAllDocuments(user_id)
-            dispatch(documentSlice.actions.setAllDocuments(documents))
-        } catch (e) {
-            console.log(e)
-        }
+    deleteDocuments: (state, action: PayloadAction<number[]>) => {
+      return state.filter((document) => !action.payload.includes(document.id));
+    },
+    addDocument: (state, action: PayloadAction<Document>) => {
+      return [...state, action.payload];
+    },
+    addAllDocuments: (state, action: PayloadAction<Document[]>) => {
+      return [...state, ...action.payload];
     }
-}
+  }
+});
+
+const thunkAddAllDocuments = (supabaseClient?: SupabaseClient): AppThunk => {
+  return async (dispatch) => {
+    dispatch(startLoading(LoadingTrigger.FetchingDocuments));
+    try {
+      const documents = await fetchAllDocuments(supabaseClient);
+      dispatch(documentSlice.actions.setAllDocuments(documents));
+    } catch (e) {
+      console.log(e);
+    } finally {
+      dispatch(endLoading(LoadingTrigger.FetchingDocuments));
+    }
+  };
+};
 
 const thunkDeleteDocuments = (document_ids: number[]): AppThunk => {
-    return async (dispatch, getState) => {
-        const documents = getState().documents.filter(document => document_ids
-            .includes(document.id))
-        if (!documents) {
-            return
-        }
-        try {
-            dispatch(documentSlice.actions.deleteDocuments(document_ids))
-            deleteDocuments(document_ids)
-        } catch (e) {
-            dispatch(documentSlice.actions.addAllDocuments(documents))
-        }
-
+  return async (dispatch, getState) => {
+    const documents = getState().documents.filter((document) =>
+      document_ids.includes(document.id)
+    );
+    if (!documents) {
+      // TODO: Display error
+      return;
     }
-}
-
-
+    dispatch(startLoading(LoadingTrigger.DeletingDocuments));
+    try {
+      dispatch(documentSlice.actions.deleteDocuments(document_ids));
+      await deleteDocuments(document_ids);
+    } catch (e) {
+      dispatch(documentSlice.actions.addAllDocuments(documents));
+    } finally {
+      dispatch(endLoading(LoadingTrigger.DeletingDocuments));
+    }
+  };
+};
 
 export const optimisticDocumentActions = {
-    fetchAllDocuments: thunkAddAllDocuments,
-    deleteDocuments: thunkDeleteDocuments,
-}
+  fetchAllDocuments: thunkAddAllDocuments,
+  deleteDocuments: thunkDeleteDocuments
+};
 
-export default documentSlice.reducer
-export const { } = documentSlice.actions
+export const getDocumentsFromStore = () =>
+  useAppSelector((state) => state.documents);
+
+export default documentSlice.reducer;
+export const { setAllDocuments } = documentSlice.actions;
