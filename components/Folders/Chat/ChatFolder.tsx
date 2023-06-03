1f7d62b30ba9678c130b23d65254438caecfc924
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-06-03 16:26:31 -0400

    Render folder icons when sidebar is closed

--- components/Folders/Chat/ChatFolder.tsx
+++ components/Folders/Chat/ChatFolder.tsx
@@ -17,6 +17,11 @@ import {
   optimisticConversationsActions
 } from '../../../context/redux/conversationsSlice';
 import { ConversationSummary } from '@/types/chat';
+import {
+  getSidebarStateFromStorage,
+  toggleSidebar
+} from '../../../context/redux/sidebarSlice';
+import { FolderIcon } from '@heroicons/react/24/outline';
 
 interface Props {
   searchTerm: string;
@@ -39,6 +44,8 @@ export const ChatFolder: FC<Props> = ({ searchTerm, currentFolder }) => {
       ),
     [conversations, currentFolder.id, searchTerm]
   );
+  const sidebarIsOpen = getSidebarStateFromStorage();
+  const openSidebar = () => dispatch(toggleSidebar());
 
   // =======================
   // Local state
@@ -112,124 +119,140 @@ export const ChatFolder: FC<Props> = ({ searchTerm, currentFolder }) => {
     e.target.style.background = 'none';
   };
 
-  return (
-    <>
-      <div className="relative flex items-center">
-        {isRenaming ? (
-          <div
-            className={classNames(
-              'flex w-full items-center bg-blackA10 gap-3 px-2 py-1 rounded-lg'
-            )}
-          >
-            {isOpen ? (
-              <IconCaretDown size={18} />
-            ) : (
-              <IconCaretRight size={18} />
-            )}
-            <input
-              className={classNames(
-                'px-2 py-2.5 mr-12 flex-1 overflow-hidden overflow-ellipsis',
-                'bg-transparent text-left text-sm leading-3 text-white outline-none',
-                'focus:ring-0 focus:outline-0 ring-0 border-none'
-              )}
-              type="text"
-              value={renameValue}
-              onChange={(e) => setRenameValue(e.target.value)}
-              onKeyDown={handleEnterDown}
-              autoFocus
-            />
-          </div>
-        ) : (
-          <button
-            className={`text-neutral-400 hover:text-neutral-100 group flex w-full cursor-pointer items-center gap-3 rounded-lg px-2 py-2.5 my-0.5 text-sm transition-colors duration-200 hover:bg-[#343541]/90`}
-            onClick={() => setIsOpen(!isOpen)}
-            onDrop={(e) => handleDrop(e, currentFolder)}
-            onDragOver={allowDrop}
-            onDragEnter={highlightDrop}
-            onDragLeave={removeHighlight}
-          >
-            {isOpen ? (
-              <IconCaretDown size={18} />
-            ) : (
-              <IconCaretRight size={18} />
-            )}
+  if (sidebarIsOpen)
+    return (
+      <>
+        <div className="relative flex items-center">
+          {isRenaming ? (
             <div
               className={classNames(
-                'text-neutral-400 group-hover:text-neutral-100 hover:text-neutral-100 relative flex-1',
-                'overflow-hidden text-ellipsis whitespace-nowrap break-all text-left text-sm leading-3'
+                'flex w-full items-center bg-blackA10 gap-3 px-2 py-1 rounded-lg'
               )}
-              onDragEnter={(e) => e.stopPropagation()}
-              onDragLeave={(e) => e.stopPropagation()}
             >
-              {currentFolder.name}
+              {isOpen ? (
+                <IconCaretDown size={18} />
+              ) : (
+                <IconCaretRight size={18} />
+              )}
+              <input
+                className={classNames(
+                  'px-2 py-2.5 mr-12 flex-1 overflow-hidden overflow-ellipsis',
+                  'bg-transparent text-left text-sm leading-3 text-white outline-none',
+                  'focus:ring-0 focus:outline-0 ring-0 border-none'
+                )}
+                type="text"
+                value={renameValue}
+                onChange={(e) => setRenameValue(e.target.value)}
+                onKeyDown={handleEnterDown}
+                autoFocus
+              />
             </div>
-          </button>
+          ) : (
+            <button
+              className={`text-neutral-400 hover:text-neutral-100 group flex w-full cursor-pointer items-center gap-3 rounded-lg px-2 py-2.5 my-0.5 text-sm transition-colors duration-200 hover:bg-[#343541]/90`}
+              onClick={() => setIsOpen(!isOpen)}
+              onDrop={(e) => handleDrop(e, currentFolder)}
+              onDragOver={allowDrop}
+              onDragEnter={highlightDrop}
+              onDragLeave={removeHighlight}
+            >
+              {isOpen ? (
+                <IconCaretDown size={18} />
+              ) : (
+                <IconCaretRight size={18} />
+              )}
+              <div
+                className={classNames(
+                  'text-neutral-400 group-hover:text-neutral-100 hover:text-neutral-100 relative flex-1',
+                  'overflow-hidden text-ellipsis whitespace-nowrap break-all text-left text-sm leading-3'
+                )}
+                onDragEnter={(e) => e.stopPropagation()}
+                onDragLeave={(e) => e.stopPropagation()}
+              >
+                {currentFolder.name}
+              </div>
+            </button>
+          )}
+          {(isDeleting || isRenaming) && (
+            <div className="absolute right-1 z-10 flex text-gray-300">
+              <button
+                className="min-w-[20px] p-1 text-neutral-400 hover:text-neutral-100"
+                onClick={async (e) => {
+                  e.stopPropagation();
+                  if (isDeleting) await handleDeleteFolder();
+                  else if (isRenaming) await handleRename();
+                }}
+              >
+                <CheckIcon
+                  className={
+                    'h-[18px] w-[18px] text-neutral-400 hover:text-neutral-100'
+                  }
+                />
+              </button>
+              <button
+                className="min-w-[20px] p-1 text-neutral-400 hover:text-neutral-100"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setIsDeleting(false);
+                  setIsRenaming(false);
+                }}
+              >
+                <XMarkIcon
+                  className={
+                    'h-[18px] w-[18px] text-neutral-400 hover:text-neutral-100'
+                  }
+                />
+              </button>
+            </div>
+          )}
+          {!isDeleting && !isRenaming && (
+            <div className="absolute right-1 z-10 flex text-gray-300">
+              <button
+                className="min-w-[20px] p-1 text-neutral-400 hover:text-neutral-100"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setIsRenaming(true);
+                  setRenameValue(currentFolder.name);
+                }}
+              >
+                <PencilIcon className={'w-[17px] h-[17px]'} strokeWidth={2} />
+              </button>
+              <button
+                className="min-w-[20px] p-1 text-neutral-400 hover:text-neutral-100"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setIsDeleting(true);
+                }}
+              >
+                <TrashIcon className={'w-[18px] h-[18px]'} strokeWidth={2} />
+              </button>
+            </div>
+          )}
+        </div>
+        {isOpen &&
+          folderConversations.map((conversation) => (
+            <div
+              key={conversation.id}
+              className={'ml-4 gap-2 border-l border-neutral-400 pl-2'}
+            >
+              <ConversationComponent conversation={conversation} />
+            </div>
+          ))}
+      </>
+    );
+  else
+    return (
+      <button
+        className={classNames(
+          'mx-auto text-neutral-400 hover:text-neutral-100',
+          'hover:bg-[#343541]/90 p-2.5 mx-[1px] rounded-lg transition-colors duration-200'
         )}
-        {(isDeleting || isRenaming) && (
-          <div className="absolute right-1 z-10 flex text-gray-300">
-            <button
-              className="min-w-[20px] p-1 text-neutral-400 hover:text-neutral-100"
-              onClick={async (e) => {
-                e.stopPropagation();
-                if (isDeleting) await handleDeleteFolder();
-                else if (isRenaming) await handleRename();
-              }}
-            >
-              <CheckIcon
-                className={
-                  'h-[18px] w-[18px] text-neutral-400 hover:text-neutral-100'
-                }
-              />
-            </button>
-            <button
-              className="min-w-[20px] p-1 text-neutral-400 hover:text-neutral-100"
-              onClick={(e) => {
-                e.stopPropagation();
-                setIsDeleting(false);
-                setIsRenaming(false);
-              }}
-            >
-              <XMarkIcon
-                className={
-                  'h-[18px] w-[18px] text-neutral-400 hover:text-neutral-100'
-                }
-              />
-            </button>
-          </div>
-        )}
-        {!isDeleting && !isRenaming && (
-          <div className="absolute right-1 z-10 flex text-gray-300">
-            <button
-              className="min-w-[20px] p-1 text-neutral-400 hover:text-neutral-100"
-              onClick={(e) => {
-                e.stopPropagation();
-                setIsRenaming(true);
-                setRenameValue(currentFolder.name);
-              }}
-            >
-              <PencilIcon className={'w-[17px] h-[17px]'} strokeWidth={2} />
-            </button>
-            <button
-              className="min-w-[20px] p-1 text-neutral-400 hover:text-neutral-100"
-              onClick={(e) => {
-                e.stopPropagation();
-                setIsDeleting(true);
-              }}
-            >
-              <TrashIcon className={'w-[18px] h-[18px]'} strokeWidth={2} />
-            </button>
-          </div>
-        )}
-      </div>
-      {isOpen &&
-        folderConversations.map((conversation) => (
-          <div
-            key={conversation.id}
-            className={'ml-4 gap-2 border-l border-neutral-400 pl-2'}
-          >
-            <ConversationComponent conversation={conversation} />
-          </div>
-        ))}
-    </>
-  );
+        onClick={() => {
+          openSidebar();
+          setIsOpen(true);
+        }}
+      >
+        <FolderIcon className={'w-5 h-5'} strokeWidth={1} />
+      </button>
+    );
 };
