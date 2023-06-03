f6255dbfed5f58469e9eb00ab8a72d14e797089d
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-06-03 13:13:59 -0400

    Pass class names via props to the sidebar button

--- components/Sidebar/SidebarButton.tsx
+++ components/Sidebar/SidebarButton.tsx
@@ -2,15 +2,22 @@ import { FC } from 'react';
 import classNames from 'classnames';
 
 interface Props {
+  className?: string;
   text: string;
   icon: JSX.Element;
   onClick: () => void;
 }
 
-export const SidebarButton: FC<Props> = ({ text, icon, onClick }) => {
+export const SidebarButton: FC<Props> = ({
+  className = '',
+  text,
+  icon,
+  onClick
+}) => {
   return (
     <button
       className={classNames(
+        className,
         'flex w-full cursor-pointer select-none items-center gap-3',
         'rounded-lg py-2.5 px-2 text-sm leading-3 text-white/75 transition-colors',
         'duration-200 hover:text-white hover:bg-gray-500/10'
