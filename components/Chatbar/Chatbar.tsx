dda16d74be645cb207fd4e45c1539260a53f7646
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-06-03 13:22:31 -0400

    Render loading animation and update styling in the chatbar component

--- components/Chatbar/Chatbar.tsx
+++ components/Chatbar/Chatbar.tsx
@@ -1,5 +1,5 @@
 import { ConversationSummary } from '@/types/chat';
-import { memo, useState } from 'react';
+import React, { memo, useMemo, useState } from 'react';
 import { IconMessagesOff } from '@tabler/icons-react';
 import { ChatFolders } from '../Folders/Chat/ChatFolders';
 import Search from '../Sidebar/Search';
@@ -15,6 +15,13 @@ import {
   optimisticConversationsActions
 } from 'context/redux/conversationsSlice';
 import { getSidebarStateFromStorage } from '../../context/redux/sidebarSlice';
+import * as Separator from '@radix-ui/react-separator';
+import useRouteChange from '../../hooks/useRouteChange';
+import { RotatingLines } from 'react-loader-spinner';
+import {
+  getLoadingStateFromStore,
+  LoadingTrigger
+} from '../../context/redux/loadingSlice';
 
 export default memo(function Chatbar() {
   // =========================
@@ -24,7 +31,26 @@ export default memo(function Chatbar() {
   const folders = getFoldersFromStorage();
   const conversations = getConversationsFromStorage();
   const sidebarOpen = getSidebarStateFromStorage();
+  const deletingConversations = getLoadingStateFromStore(
+    LoadingTrigger.DeletingConversations
+  );
+
+  // ======= Filtering Conversations =======
   const [searchTerm, setSearchTerm] = useState('');
+  const filteredConversations = useMemo(
+    () =>
+      conversations.filter((conversation) => {
+        // Select conversations without a folder
+        if (conversation.folderId) return false;
+
+        const searchable = conversation.name.toLocaleLowerCase();
+        return searchable.toLowerCase().includes(searchTerm.toLowerCase());
+      }),
+    [searchTerm, conversations]
+  );
+
+  // ===== Detect Page Loading =====
+  const [pageLoading] = useRouteChange();
 
   // =========================
   // Handlers
@@ -61,51 +87,85 @@ export default memo(function Chatbar() {
     e.target.style.background = 'none';
   };
 
+  // ======== Tailwind Classes ========
+  const separatorStyle = classNames(
+    'bg-neutral6 data-[orientation=horizontal]:h-px',
+    'data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full',
+    'data-[orientation=vertical]:w-px'
+  );
+
   return (
     <motion.div
       className={classNames(
-        'flex flex-col w-full mt-2 mb-1',
-        'space-y-2 transition-all duration-150 transform-gpu'
+        'flex flex-col w-full mt-2 mb-1 relative',
+        'transition-all duration-150 transform-gpu'
       )}
       animate={sidebarOpen ? 'open' : 'closed'}
     >
+      {/* ========== Loading Spinner ========== */}
+      {(pageLoading || deletingConversations) && (
+        <div
+          className={classNames(
+            'absolute top-0 left-0 right-0 bottom-0 z-20',
+            'flex items-center justify-center bg-zinc-900'
+          )}
+        >
+          <RotatingLines
+            strokeColor="#9ca3af"
+            strokeWidth="2"
+            animationDuration="1"
+            width="2rem"
+            visible={true}
+          />
+        </div>
+      )}
       {/* ========== Conversations Search Bar ========== */}
       {conversations.length > 1 && (
-        <Search
-          searchTerm={searchTerm}
-          setSearchTerm={(value) => setSearchTerm(value)}
-        />
+        <>
+          <Search
+            searchTerm={searchTerm}
+            setSearchTerm={(value) => setSearchTerm(value)}
+          />
+          <Separator.Root className={classNames(separatorStyle, 'mt-3')} />
+        </>
       )}
       {/* ========== Chat Folders List ========== */}
-      <div className="flex-grow overflow-auto">
-        {folders.length > 0 && (
-          <div className="flex border-b border-neutral6 pb-2">
-            <ChatFolders searchTerm={searchTerm} />
-          </div>
-        )}
-        {/* ==========  List of Conversations Without Folders ========== */}
-        {conversations.length > 0 ? (
-          <div
-            className="pt-2 border-b border-neutral6 pb-2"
-            onDrop={(e) => handleDrop(e)}
-            onDragOver={allowDrop}
-            onDragEnter={highlightDrop}
-            onDragLeave={removeHighlight}
-          >
-            <Conversations searchTerm={searchTerm} />
-          </div>
-        ) : (
+      {folders.length > 0 && (
+        <>
+          <ChatFolders searchTerm={searchTerm} />
+          <Separator.Root className={separatorStyle} />
+        </>
+      )}
+      {/* ==========  List of Conversations Without Folders ========== */}
+      {conversations.length > 0 ? (
+        filteredConversations.length > 0 ? (
+          <>
+            <div
+              className={'flex justify-center items-center my-2'}
+              onDrop={(e) => handleDrop(e)}
+              onDragOver={allowDrop}
+              onDragEnter={highlightDrop}
+              onDragLeave={removeHighlight}
+            >
+              <Conversations conversations={filteredConversations} />
+            </div>
+            <Separator.Root className={separatorStyle} />
+          </>
+        ) : null
+      ) : (
+        <>
           <div
             className={classNames(
-              'flex flex-col gap-3 items-center',
-              'text-sm leading-normal mt-4 mb-1 text-white opacity-50'
+              'flex flex-col items-center py-8',
+              'text-sm leading-normal text-white opacity-50'
             )}
           >
             <IconMessagesOff />
             No conversations.
           </div>
-        )}
-      </div>
+          <Separator.Root className={classNames(separatorStyle)} />
+        </>
+      )}
       <ChatbarSettings />
     </motion.div>
   );
