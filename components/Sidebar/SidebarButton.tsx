10e1338a90c43fb36203350b5e45c68f215b27f7
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-06-04 18:53:38 -0400

    Update hover color in the SidebarButton.tsx

--- components/Sidebar/SidebarButton.tsx
+++ components/Sidebar/SidebarButton.tsx
@@ -19,8 +19,8 @@ export const SidebarButton: FC<Props> = ({
       className={classNames(
         className,
         'flex w-full cursor-pointer select-none items-center gap-3',
-        'rounded-lg py-2.5 px-2 text-sm leading-3 text-white/75 transition-colors',
-        'duration-200 hover:text-white hover:bg-gray-500/10',
+        'rounded-lg py-2.5 px-2 text-sm leading-3 text-neutral-400 transition-colors',
+        'duration-200 hover:text-neutral-100 hover:bg-gray-500/10',
         !text && 'justify-center'
       )}
       onClick={onClick}
