f824763d9d835a60482691f0908deba3458c4efb
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-06-22 23:37:10 -0400

    Update Header.tsx styling

--- components/ui/Header/Header.tsx
+++ components/ui/Header/Header.tsx
@@ -54,21 +54,25 @@ export default memo(function Header() {
       {sideBarOpen && (
         <div
           // Overlay to hide chat when sidebar is open on mobile
-          className={
-            'absolute sm:hidden z-20 top-0 right-0 left-0 bottom-0 bg-gray-100 dark:bg-neutral6 backdrop-blur-xl'
-          }
+          className={classNames(
+            'absolute sm:hidden top-0 right-0 left-0 bottom-0',
+            'bg-gray-100 dark:bg-neutral6 backdrop-blur-xl',
+            sideBarOpen ? 'z-30' : 'z-20'
+          )}
         />
       )}
       <header
         className={classNames(
           'flex flex-row items-center justify-between min-h-[4.5rem] h-[4.5rem] py-3 sm:border-b',
-          'border-gray-200 ',
+          'border-gray-200',
           'dark:border-zinc-700',
           !sideBarOpen && 'border-b',
           // Apply bottom shadow only on the files page
           router.pathname === '/files' && headerShadow,
-          'overflow-visible z-20 relative',
-          'pl-8 sm:pr-6'
+          'overflow-visible relative',
+          'pl-8 sm:pr-6',
+          sideBarOpen ? 'z-30' : 'z-20',
+          'sm:z-20'
         )}
       >
         <h1 className="text-2xl font-semibold text-gray-900 dark:text-white">
@@ -94,9 +98,9 @@ export default memo(function Header() {
             )}
           />
         </button>
-        {/* Document Filter Button */}
         {router.pathname === '/chat' && (
           <button
+            // Document Filter Button
             className={classNames(
               'hidden lg:flex group shadow rounded-full w-9 h-9 items-center justify-center',
               'bg-white dark:bg-zinc-700 hover:bg-gray-100 active:bg-gray-100 dark:hover:bg-neutral4/70',
