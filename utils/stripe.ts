5b6b9e29aca92c2a5849dc05b88215d9a91e46c1
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-07-15 13:52:13 -0400

    Update app information in the stripe.ts

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -8,8 +8,7 @@ export const stripe = new Stripe(
     // Register this as an official Stripe plugin.
     // https://stripe.com/docs/building-plugins#setappinfo
     appInfo: {
-      name: 'Next.js Subscription Starter',
-      version: '0.1.0'
+      name: 'BrainBot'
     }
   }
 );
