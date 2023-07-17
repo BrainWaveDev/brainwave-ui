ac1c18dbd126c5bae2cc92d010b3d2c8b63d0a14
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-07-16 21:54:15 -0400

    Clean up and refactor client and server Supabase actors

--- utils/supabase-admin.ts
+++ utils/supabase-admin.ts
@@ -5,10 +5,8 @@ import { toDateTime } from './helpers';
 import type { Database } from 'types/supabase';
 import { GetServerSidePropsContext } from 'next';
 import { createPagesServerClient } from '@supabase/auth-helpers-nextjs';
-import { Price, Product } from '../types';
+import { Price, Product } from '@/types/products';
 
-// Note: supabaseAdmin uses the SERVICE_ROLE_KEY which you must only use in a secure server-side context
-// as it has admin privileges and overwrites RLS policies!
 const supabaseAdmin = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL || '',
   process.env.SUPABASE_SERVICE_ROLE_KEY || ''
--- utils/supabase-client.ts
+++ utils/supabase-client.ts
@@ -1,6 +1,4 @@
 import { createPagesBrowserClient, User } from '@supabase/auth-helpers-nextjs';
-
-import { ProductWithPrice } from '../types';
 import { Document } from '@/types/document';
 import type { Database } from 'types/supabase';
 
@@ -16,24 +14,6 @@ export const getDocuments = async (): Promise<Document[]> => {
   if (error) {
     console.log(error.message);
   }
-  return (data as any) || [];
-};
-
-export const getActiveProductsWithPrices = async (): Promise<
-  ProductWithPrice[]
-> => {
-  const { data, error } = await supabase
-    .from('products')
-    .select('*, prices(*)')
-    .eq('active', true)
-    .eq('prices.active', true)
-    .order('metadata->index')
-    .order('unit_amount', { foreignTable: 'prices' });
-
-  if (error) {
-    console.log(error.message);
-  }
-  // TODO: improve the typing here.
   return (data as any) || [];
 };
 
