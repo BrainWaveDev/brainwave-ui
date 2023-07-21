6355366b03d83c22b5db59d4bd0aa8978119428b
Vasily Inkovskiy <76574837+xxArchitect@users.noreply.github.com>
2023-07-20 22:50:45 -0400

    Define helper methods and Stripe keys in stripe.ts

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -1,11 +1,18 @@
 import Stripe from 'stripe';
+import { loadStripe, Stripe as StripeClient } from '@stripe/stripe-js';
 
 const inDevEnv = process.env.NODE_ENV === 'development';
-const stripeApiKey =
+const stripeSecretKey =
   (inDevEnv
     ? process.env.STRIPE_SECRET_KEY
     : process.env.STRIPE_SECRET_KEY_LIVE) ?? '';
-export const stripe = new Stripe(stripeApiKey, {
+const stripePublishableKey =
+  (inDevEnv
+    ? process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+    : process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_LIVE) ?? '';
+let stripePromise: Promise<StripeClient | null>;
+
+export const stripe = new Stripe(stripeSecretKey, {
   // https://github.com/stripe/stripe-node#configuration
   apiVersion: '2022-11-15',
   // Register this as an official Stripe plugin.
@@ -14,5 +21,12 @@ export const stripe = new Stripe(stripeApiKey, {
     name: 'BrainBot'
   }
 });
+
+export const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(stripePublishableKey);
+  }
+  return stripePromise;
+};
 
 export default stripe;
